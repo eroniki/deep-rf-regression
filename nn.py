6f2d7a62bc62810e8ba120a61f49db84178d7d18
murat ambarkutuk <m.ambarkutuk@gmail.com>
2017-01-19 20:01:55 -0500

    Add a feature to handle SIGINT, test with other optimizers

--- nn.py
+++ nn.py
@@ -1,35 +1,53 @@
 #!/usr/bin/python
 import os
 import time
-import json
+import signal
+import sys
+import shutil
 
 from keras.models import Sequential, load_model
 from keras.layers import Dense, Dropout, Activation
-from keras.optimizers import SGD, Adagrad
-
+from keras.optimizers import SGD, Adagrad, RMSprop, Adam
 from keras import backend as K
+from keras.regularizers import l2, activity_l2
+from keras.callbacks import Callback, TensorBoard
 
 import numpy as np
+from matplotlib import pyplot as plt
 
 class nn(object):
     """docstring for nn."""
-    def __init__(self, model_name, location, valid_split):
+    def __init__(self, model_name, location, valid_split, dropout=0.2, epoch=2000, batch_size=512):
     # def __init__(self, depth, dims, activations, epoch, model_name, location):
         super(nn, self).__init__()
         self.model = None
-        self.optimizer = None
         self.full_file = None
         self.sgd = SGD(lr=0.1, decay=1e-6, momentum=0.9, nesterov=True)
         self.ada = Adagrad(lr=0.01, epsilon=1e-08, decay=0.0)
+        self.adam = Adam()
+        self.rmsprop = RMSprop(lr=0.01, clipnorm=1., decay=0.0001)
+        self.optimizer = self.sgd
         self.model_name = model_name
         self.model_location = location
         self.model_exists = False
         self.answer = "n"
         self.valid_split = valid_split
+        self.epoch = epoch
+        self.batch_size = batch_size
+        self.dropout = dropout
         # self.loss_function = loss_function
+        self.regularizer = l2(0.01)
+        self.regularizer = None
+        self.log_dir = "./logs"
+
+        self.callbacks = [
+            TensorBoard(log_dir=self.log_dir, histogram_freq=0, write_graph=True, write_images=False)
+        ]
 
         self.full_file = self.construct_full_location(self.model_location, self.model_name)
-        print self.full_file, self.check_model_exists(self.full_file)
+
+        signal.signal(signal.SIGINT, self.signal_handler)
+
         if self.check_model_exists(self.full_file):
             print "Model already exists!"
             self.answer = self.prompt_user("The model already exists, would you like to override? Y/n\n")
@@ -43,58 +61,84 @@ class nn(object):
                 self.model = self.load_model(self.full_file)
         else:
             self.model_exists = False
+            self.init_model()
+            self.init_layers()
+            self.compile_model()
 
     def init_model(self):
         self.model = Sequential()
 
+    # TODO: Hard-coded layer structure should be relaxed!
     def init_layers(self):
-        self.model.add(Dense(256, input_dim=132, init='uniform'))
-        self.model.add(Activation('relu'))
+        self.model.add(Dense(500, input_dim=132, init='he_normal', activation='relu', W_regularizer=self.regularizer, b_regularizer=self.regularizer))
         self.model.add(Dropout(0.5))
-        self.model.add(Dense(128, input_dim=132, init='uniform'))
-        self.model.add(Activation('relu'))
+        for i in range(132, 6, -3):
+            self.model.add(Dense(i, input_dim=132, init='he_normal', activation='relu', W_regularizer=self.regularizer, b_regularizer=self.regularizer))
+            self.model.add(Dropout(0.5))
+
+
+        self.model.add(Dense(5, init='he_normal', activation='relu', W_regularizer=self.regularizer, b_regularizer=self.regularizer))
         self.model.add(Dropout(0.5))
-        self.model.add(Dense(64, input_dim=132, init='uniform'))
-        self.model.add(Activation('relu'))
+        self.model.add(Dense(4, init='he_normal', activation='relu', W_regularizer=self.regularizer, b_regularizer=self.regularizer))
         self.model.add(Dropout(0.5))
-        self.model.add(Dense(32, init='uniform'))
-        self.model.add(Activation('relu'))
+        self.model.add(Dense(3, init='he_normal', activation='relu', W_regularizer=self.regularizer, b_regularizer=self.regularizer))
         self.model.add(Dropout(0.5))
-        self.model.add(Dense(16, init='uniform'))
-        self.model.add(Activation('relu'))
-        self.model.add(Dropout(0.5))
-        self.model.add(Dense(8, init='uniform'))
-        self.model.add(Activation('relu'))
-        self.model.add(Dropout(0.5))
-        self.model.add(Dense(4, init='uniform'))
-        self.model.add(Activation('relu'))
-        self.model.add(Dropout(0.5))
-        self.model.add(Dense(2, init='uniform'))
-        self.model.add(Activation('relu'))
+        self.model.add(Dense(2, init='he_normal', activation='relu', W_regularizer=self.regularizer, b_regularizer=self.regularizer))
+
+    def signal_handler(self, signal, frame):
+        shutil.rmtree(self.log_dir, ignore_errors=False, onerror=None)
+        # os.rmdir(self.log_dir, *, dir_fd=None)
+        print "Clean-up done. Gracefully exited"
+        sys.exit(0)
 
     def prompt_user(self, prompt):
         return raw_input(prompt)
 
+    def plot_cdf(self, error, nbins):
+        hist, bins = self.create_error_histogram(error, nbins)
+        cdf = np.true_divide(np.cumsum(hist), np.sum(hist))
+        plt.plot(bins[1:], cdf)
+        plt.grid()
+        plt.show()
+
+    def create_error_histogram(self, error, nbins):
+        hist, bins = np.histogram(error, nbins)
+        return hist, bins
 
     def compile_model(self):
-        self.model.compile(loss=self.localization_loss,
-        # self.model.compile(loss='mean_squared_error',
-                      optimizer=self.sgd)
+        # self.model.compile(loss=self.localization_loss, optimizer=self.optimizer)
+        self.model.compile(loss=self.localization_loss, optimizer=self.optimizer)
 
     def fit_model(self, x_train, y_train):
-        self.model.fit(x_train, y_train, nb_epoch=200, batch_size=500, verbose=2, shuffle=False, validation_split = self.valid_split)
+        self.model.fit(x_train, y_train, nb_epoch=self.epoch, batch_size=self.batch_size, verbose=2, shuffle=True, validation_split = self.valid_split, callbacks=self.callbacks)
 
     def evaluate_model(self, x_valid, y_valid):
-        return self.model.evaluate(x_valid, y_valid, batch_size=500)
+        return self.model.evaluate(x_valid, y_valid, batch_size=self.batch_size)
 
     def predict(self, x):
-        return self.model.predict(x, batch_size=500, verbose=1)
+        return self.model.predict(x, batch_size=self.batch_size, verbose=1)
 
-    def localization_loss(self, y, y_pred):
-        error = y-y_pred
+    def calculate_error(self, y, y_hat):
+        error = y-y_hat
         e_x = error[:, 0]
         e_y = error[:, 1]
-        return K.mean(e_x**2+e_y**2)
+        return error, e_x, e_y
+
+    def residual_analysis(self, y, y_hat, plot_cdf=True, nbins=10):
+        error, e_x, e_y = self.calculate_error(y, y_hat)
+        mse_x = np.mean(np.sqrt(e_x**2), axis=0)
+        mse_y = np.mean(np.sqrt(e_y**2), axis=0)
+        mse = np.mean(np.sqrt(e_x**2+e_y**2))
+
+        if plot_cdf:
+            self.plot_cdf(np.sqrt(e_x**2+e_y**2), nbins)
+
+        return mse, mse_x, mse_y
+
+    def localization_loss(self, y, y_hat):
+        error, e_x, e_y = self.calculate_error(y, y_hat)
+        # return K.mean(np.exp(e_x**2+e_y**2))
+        return K.prod((e_x**2+e_y**2))
 
     def create_model(self, arg):
         pass
@@ -106,6 +150,8 @@ class nn(object):
         return load_model(full_file)
 
     def save_model(self, folder_location, fname):
+        timestamp = int(time.time())
+        fname = str(timestamp) + "_" + fname
         full_file = self.construct_full_location(folder_location, fname)
         self.model.save(full_file)
 
@@ -123,6 +169,5 @@ class nn(object):
 def main():
     pass
 
-# DNNRegressor
 if __name__ == "__main__":
     main()
